b9d62423a53642b233f675aba2077e85b12aaa52
Glaucielle Sá <glauciellesa@gmail.com>
2023-02-02 12:17:44 +0100

    fix NavLink

--- src/menu/Header.tsx
+++ src/menu/Header.tsx
@@ -1,13 +1,14 @@
 import reactLogo from "../assets/react.svg";
 import Navbar from "./Navbar";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <div className="nav-area">
-        <a href="/" className="logo">
+        <NavLink to="/" className="logo">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </NavLink>
         <Navbar />
       </div>
     </header>
